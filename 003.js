98f16f3c39ad2b0ea411f8413e4437b5409f7406
trizoza <peter.trizuliak@gmail.com>
2020-08-07 13:23:35 +0000

    fix function to find primes

--- 003.js
+++ 003.js
@@ -1,12 +1,27 @@
 const isPrime = (num) => {
     if (typeof num !== "number") return false
     if (num < 2) return false
-    const primes = []
+    const notPrimes = []
     for (let i = 2; i <= num; i++) {
-        if (i !== num && num%i === 0 && num/1 > 1) primes.push(num)
+        if (i !== num && num%i === 0 && num/1 > 1) notPrimes.push(num)
+        if (notPrimes.length) i = num
     }
-    return primes.length ? false : true
+    return notPrimes.length ? false : true
 }
 
-let check = isPrime(3);
-console.log(check);
+// let check = isPrime(3);
+// console.log(check);
+
+const findPrimes = num => {
+    const primes = []
+    for (let i =2; i <= num; i++) {
+        if (isPrime(i)) {
+            primes.push(i)
+            console.log(i)
+        }
+    }
+    console.log(primes)
+    return primes
+}
+
+const candidates = findPrimes(1000);
